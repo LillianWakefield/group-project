05ff81f07ae96468fe38c8e5ba966aeabbf8663a
Lillian Wakefield <37185466+LillianWakefield@users.noreply.github.com>
2018-05-09 09:51:53 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
-    $('checkBoxes').on("click", "startButton", function() { 
+    function renderBoth(left,right){
+        $('.card-left').append(left);
+        $('.card-right').append(right);
+    }
+    $('.startButton').click(function(){
+        if ($('#both-check').is(':checked')){
+            $.get('https://dog.ceo/api/breeds/image/random', function(data){
+            var dogPic = $('.card-left').append('<image src='+data.message+'/>');
+            });
+            var catPic = $('.card-right').append('<image src="http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"/>')
+        }
+        else if($('#dogs-check').is(':checked')){
+            $.get('https://dog.ceo/api/breeds/image/random', function(data){
+                $('.card-left').append('<image src='+data.message+'/>');
+            });
+            $.get('https://dog.ceo/api/breeds/image/random', function(data){
+                $('.card-right').append('<image src='+data.message+'/>');
+            });   
+        }
+        else if ($('#cats-check').is(':checked')){
+            $('.card-left').append('<image src="http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"/>')
+            $('.card-right').append('<image src="http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"/>')
+        }
 
     });
-
-    $('cuteContainer').on("click", "cutePick", function() {
-
-    });
-
-});
+});  
