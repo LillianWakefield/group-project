7be68bd6e7340dedd03e968728ba9c48e9a1bb53
Lillian Wakefield <37185466+LillianWakefield@users.noreply.github.com>
2018-05-09 21:00:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
-    function renderBoth(left,right){
-        $('.card-left').append(left);
-        $('.card-right').append(right);
-    }
+    //Renders images to the screen when page first loads.
+    $.get('https://dog.ceo/api/breeds/image/random', function(data){
+            $('#card-left').attr('src', data.message);
+            });
+            
+            $('#card-right').attr('src', "http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"+ new Date().getTime())
+    //Depending on the user preference, loads images to screen with submit button.
     $('.startButton').click(function(){
         if ($('#both-check').is(':checked')){
             $.get('https://dog.ceo/api/breeds/image/random', function(data){
-            var dogPic = $('.card-left').append('<image src='+data.message+'/>');
+            $('#card-left').attr('src', data.message);
             });
-            var catPic = $('.card-right').append('<image src="http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"/>')
+            
+            $('#card-right').attr('src', "http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"+ new Date().getTime())
         }
         else if($('#dogs-check').is(':checked')){
             $.get('https://dog.ceo/api/breeds/image/random', function(data){
-                $('.card-left').append('<image src='+data.message+'/>');
+                $('#card-left').attr('src', data.message);;
             });
             $.get('https://dog.ceo/api/breeds/image/random', function(data){
-                $('.card-right').append('<image src='+data.message+'/>');
+                $('#card-right').attr('src', data.message);;
             });   
         }
         else if ($('#cats-check').is(':checked')){
-            $('.card-left').append('<image src="http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"/>')
-            $('.card-right').append('<image src="http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"/>')
+            $('#card-left').attr('src', "http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"+ new Date().getTime())
+            $('#card-right').attr('src', "http://thecatapi.com/api/images/get?format=src&type=jpg,png&size=med"+ new Date().getTime()) 
         }
 
     });
